466c0a1b1c2a136386a819ad16247674608fd936
Arsalan <arsal243@gmail.com>
2026-05-15 17:20:30 +0500

    added negative test cases of login scenario

---
+++ tests/TC-002loginWithValidUsername.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+
+test('@Testcase - 002 login with invalid username', async ({page}) => {
+    await page.goto('https://practicetestautomation.com/practice-test-login/')
+    await expect(page.getByRole('textbox', { name: 'Username'})).toBeVisible()
+    await page.getByLabel('Username').fill('incorrectUser')
+    await expect(page.getByRole('textbox', { name: 'Password'})).toBeVisible()
+    await page.getByLabel('Password').fill('Password123')
+    await expect(page.getByRole('button', { name: 'Submit'})).toBeEnabled()
+    await page.getByRole('button', { name: 'Submit'}).click();
+    await expect(page.getByText('Your username is invalid!')).toBeVisible()
+
+  }) 
