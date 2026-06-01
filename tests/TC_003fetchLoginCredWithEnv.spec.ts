331332c58106f009440515f288c0a9fc1f865426
Arsalan <arsal243@gmail.com>
2026-06-01 15:05:20 +0500

    TC-004 added and function tests

---
+++ tests/TC_003fetchLoginCredWithEnv.spec.ts
@@ -0,0 +1,47 @@
+import { test, expect, Page} from '@playwright/test'
+import { USERNAME, PASSWORD } from '../Fixtures/constants';
+import * as dotenv from 'dotenv'
+const fs = require('fs')
+
+dotenv.config();
+
+const data = JSON.parse(
+    fs.readFileSync('./Fixtures/url.json', 'utf8')
+)
+
+test.describe("fetch login cred from env file", ()=> {
+    test('Login Functionality', async ({ page }) =>{
+
+         //navigate to loginPage
+                await navigateToLoginPage(page);
+        
+                //fill the value of login
+                await fillLoginForm(page, process.env.USERNAME!, process.env.PASSWORD!);
+        
+                //submit form
+                await submitLoginForm(page);
+        
+                //validate login success msgs
+                await verifySucessfulLogin(page);
+        
+            })
+    })
+
+    async function navigateToLoginPage(page : Page) {
+        await page.goto(data.baseUrl)
+    }
+
+    async function fillLoginForm(page : Page, username: string, password: string) {
+        await page.getByRole('textbox', {name: 'Username' }).fill(username)
+        await page.getByRole('textbox', {name: 'Password' }).fill(password)
+    }
+    
+    async function submitLoginForm(page: Page) {
+        await page.getByRole('button', { name: 'Submit' }).click()
+    }
+    
+    async function verifySucessfulLogin(page : Page) {
+        await expect(page.getByRole('textbox', { name: 'Username'})).toBeVisible()
+        await expect(page.getByRole('textbox', { name: 'Password'})).toBeVisible()
+        await expect(page.getByRole('button', { name: 'Submit'})).toBeEnabled()
+    }
